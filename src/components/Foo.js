9181e63be688cbc87af3696571f3c823cac2765e
David Adams <dadams@clearc2.com>
2016-12-07 09:25:54 -0600

    initial commit

---
+++ src/components/Foo.js
@@ -0,0 +1,6 @@
+import Bar from './Bar'
+
+export default function () {
+  console.log('Foo.js')
+  Bar()
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import Foo from './components/Foo'
+import Bar from './components/Bar'
+
+Foo()
