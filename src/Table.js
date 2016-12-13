daaef01afe7f0b588128a218a289fe309c4349fc
David Adams <dadams@clearc2.com>
2016-12-13 09:21:42 -0600

    built package

--- src/Table.js
+++ src/Table.js
@@ -1,7 +1,10 @@
 import Foo from './components/Foo'
 import Bar from './components/Bar'
 
+import {Table, Column, ColumnGroup} from './components/Table'
+
 export {
-  Foo,
-  Bar
+  Table,
+  Column,
+  ColumnGroup
 }
